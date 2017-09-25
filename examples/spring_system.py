23c1f5544c5798b0062c1c3f9333b4e98bc4a390
Tomasz Koziara <t.koziara@gmail.com>
2017-09-25 13:15:42 +0200

    springs system example continued

--- examples/spring_system.py
+++ examples/spring_system.py
@@ -16,16 +16,16 @@ from progress_bar import *
 from scipy import spatial
 import numpy as np
 
-nx = 20
+nx = 10
 ny = 5
 nz = 5
-radius = 1.
-dura = 0.001
+radius = 2.
+dura = 5.
 
 # materials
 matnum = MATERIAL (1E3, 1E9, 0.25)
-spring = [-1,-1E7, 1,1E7]
-dashpot = [-1, -8E5, 1, 8E5]
+spring = [-1,-1E6, 1,1E6]
+dashpot = [-1, -1E2, 1, 1E2]
 
 # (nx,ny,nz) array of unit cubes
 iend = nx*ny*nz-1
@@ -35,30 +35,29 @@ data = zip(x.ravel(), y.ravel(), z.ravel())
 datarange = range (0, len(data))
 for i in datarange:
   p = data[i]
-  nodes = [p[0], p[1], p[2],
-	   p[0]+1, p[1], p[2],
-	   p[0]+1, p[1]+1, p[2],
-	   p[0], p[1]+1, p[2],
-	   p[0], p[1], p[2]+1,
-	   p[0]+1, p[1], p[2]+1,
-	   p[0]+1, p[1]+1, p[2]+1,
-	   p[0], p[1]+1, p[2]+1]
+  nodes = [p[0]-.5, p[1]-.5, p[2]-.5,
+	   p[0]+.5, p[1]-.5, p[2]-.5,
+	   p[0]+.5, p[1]+.5, p[2]-.5,
+	   p[0]-.5, p[1]+.5, p[2]-.5,
+	   p[0]-.5, p[1]-.5, p[2]+.5,
+	   p[0]+.5, p[1]-.5, p[2]+.5,
+	   p[0]+.5, p[1]+.5, p[2]+.5,
+	   p[0]-.5, p[1]+.5, p[2]+.5]
   elements = [8, 0, 1, 2, 3, 4, 5, 6, 7, matnum]
   parnum = MESH (nodes, elements, matnum, 0)
   progress_bar(i, iend, 'Adding particles:')
 
 # randomized springs within radius
-def add(a,b): return (a[0]+b[0],a[1]+b[2],a[2]+b[2])
+def add(a,b): return (a[0]+b[0],a[1]+b[1],a[2]+b[2])
 def sub(a,b): return (a[0]-b[0],a[1]-b[2],a[2]-b[2])
 def mul(a,b): return (a[0]*b,a[1]*b,a[2]*b)
 progress_bar(0, iend, 'Adding springs:')
 tree = spatial.KDTree(data)
-o5 = (.5,.5,.5)
 for i in datarange:
-   adj = tree.query_ball_point(np.array(data[i]), radius)
-   for j in adj:
-     p = add(data[i],o5)
-     q = add(data[j],o5)
+   p = data[i]
+   adj = tree.query_ball_point(np.array(p), radius)
+   for j in [k for k in adj if k < i]:
+     q = data[j]
      x = mul(add(p,q),.5)
      sprnum = SPRING (i, x, j, x, spring, dashpot)
    progress_bar(i, iend, 'Adding springs:')
@@ -70,7 +69,7 @@ for i in datarange[-ny*nz:]:
 GRAVITY (0., 0., -10.)
 
 hcri = CRITICAL()
-step = 0.01 * hcri
+step = 0.1 * hcri
 print '%d particles and %d springs' % (parnum, sprnum)
 print 'Critical step estimated as %g' % hcri
 print 'Running %d steps of size %g:' % (int(dura/step), step)
