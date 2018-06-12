cc0b5dfbac67d35e38aa0f3d4dda4d67d523c493
Tomasz Koziara <t.koziara@gmail.com>
2018-06-12 10:58:52 +0200

    angledot fix; spherical joint example iteration

--- tests/spherical_joint.py
+++ tests/spherical_joint.py
@@ -14,35 +14,32 @@ nodes2 = [0.25, 0.25, 1,
           0.75, 0.25, 1,
 	  0.75, 0.75, 1,
 	  0.25, 0.75, 1,
-	  0.25, 0.25, 4,
-	  0.75, 0.25, 4,
-	  0.75, 0.75, 4,
-	  0.25, 0.75, 4]
+	  0.25, 0.25, 3,
+	  0.75, 0.25, 3,
+	  0.75, 0.75, 3,
+	  0.25, 0.75, 3]
 elements = [8, 0, 1, 2, 3, 4, 5, 6, 7, matnum]
 colors = [1, 4, 0, 1, 2, 3, 2, 4, 4, 5, 6, 7, 3]
 
 part1 = MESH (nodes1, elements, matnum, colors)
 part2 = MESH (nodes2, elements, matnum, colors)
 
-# fix part1 at 0-level
-p1 = tuple(nodes1[0:3])
-p2 = tuple(nodes1[3:6])
-p3 = tuple(nodes1[6:9])
-p4 = tuple(nodes1[9:12])
-SPRING (part1, p1, -1, p1, spring=[-1, -1E6, 1, 1E6], dashpot=1.0)
-SPRING (part1, p2, -1, p2, spring=[-1, -1E6, 1, 1E6], dashpot=1.0)
-SPRING (part1, p3, -1, p3, spring=[-1, -1E6, 1, 1E6], dashpot=1.0)
-SPRING (part1, p4, -1, p4, spring=[-1, -1E6, 1, 1E6], dashpot=1.0)
+# perscribe part1 motion
+v0 = 1.
+vx = TSERIES ([0, v0, 2.49, v0, 2.50, -v0, 4.99, -v0, 5.0,  v0, 7.49, v0, 7.5, -v0, 9.00, -v0, 10, 0, 20, 0])
+vy = TSERIES ([0, 0, 9.99, 0, 10, v0, 12.49, v0, 12.5, -v0, 14.99, -v0, 15, v0, 17.49, v0, 17.5, -v0, 20, -v0])
+vz = TSERIES ([0, 0, 20, 0])
+PRESCRIBE (part1, linear=(vx, vy, vz))
+RESTRAIN (part1, angular=[1, 0, 0, 0, 1, 0, 0, 0, 1])
 
 # spherical joint at point (0.5,0.5,1)
-ktrq=1E6
-dtrq=1
+ktrq=5E6
+dtrq=2
 SPRING (part1, (0.5,0.5,1), part2, (0.5,0.5,1), spring=[-1, -1E6, 1, 1E6], dashpot=1.0)
-TORSION_SPRING (part1, part2, (0, 0, 1), (1, 0, 0),
-  kroll=[-2, ktrq, -1, 0, 1, 0, 2, -ktrq], droll=dtrq, # allow for some freedom ... (--> cone)
+TORSION_SPRING (part2, part1, (0, 0, 1), (1, 0, 0),
+  kroll=[-1, ktrq, -0.25, 0, 0.25, 0, 1, -ktrq], droll=dtrq, # allow for some freedom ... (--> cone)
   kyaw=[-1, ktrq, 1, -ktrq], dyaw=dtrq, # block yaw rotation
   cone = ('roll', 'pitch')) # ... in the (roll, pitch) space
-VELOCITY (part2, angular=(0.1, 0.2, 0.0))
 
 GRAVITY (0., 0., -10.)
 
@@ -51,11 +48,11 @@ ox = HISTORY ('OX', part2)
 oy = HISTORY ('OY', part2)
 oz = HISTORY ('OZ', part2)
 
-h = 0.1 * CRITICAL()
+h = 7E-4
 
 print 'Time step:', h
 
-DEM (15.0, h, (0.05, h))
+DEM (20., h, (0.1, 0.001))
 
 print 'Generating plots ...'
 
